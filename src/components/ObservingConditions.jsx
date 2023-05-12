4d6b710c331aeb0aee4d2af80abb78c230083176
aneeshp4 <69907361+aneeshp4@users.noreply.github.com>
2023-05-11 23:16:00 -0400

    Added weather functionality

--- src/components/ObservingConditions.jsx
+++ src/components/ObservingConditions.jsx
@@ -1,77 +1,84 @@
 import React from "react";
 import axios from "axios";
+import Weather from "./Weather";
 import { useState, useEffect } from "react";
 
 function ObservingConditions(props) {
   const [zipCode, setZipCode] = useState("03755");
+  const [latitude, setLatitude] = useState(43);
+  const [longitude, setLongitutde] = useState(-72);
+  const [timeZone, setTimeZone] = useState("America/New_York");
   const [visibleObjects, setVisibleObjects] = useState([]);
 
   const mapbox_url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${zipCode}.json?access_token=${process.env.REACT_APP_MAPBOX_KEY}`;
 
   useEffect(() => {
-    if (zipCode.length !== 5) {
-      return
-    }
-    // Get the latitude and longitude of the zip code using mapbox API
-    axios
-      .get(mapbox_url)
-      .then((response) => {
-        const location = response.data.features[0].center;
+    const fetchData = async () => {
+      try {
+        if (zipCode.length !== 5) {
+          return;
+        }
+
+        // Get the latitude and longitude of the zip code using mapbox API
+        const mapBoxRequest = await axios.get(mapbox_url);
+        const location = mapBoxRequest.data.features[0].center;
         const lng = Math.floor(location[0]);
         const lat = Math.floor(location[1]);
+        setLatitude(lat);
+        setLongitutde(lng);
         console.log(Math.floor(Date.now() / 1000));
 
-        const googleMapsURL = `https://maps.googleapis.com/maps/api/timezone/json?location=${lat},${lng}&timestamp=${Math.floor(Date.now() / 1000)}&key=${process.env.REACT_APP_GOOGLE_MAPS_KEY}`;
+        const googleMapsURL = `https://maps.googleapis.com/maps/api/timezone/json?location=${lat},${lng}&timestamp=${Math.floor(
+          Date.now() / 1000
+        )}&key=${process.env.REACT_APP_GOOGLE_MAPS_KEY}`;
+
         // Get the current time at the zip code using timezonedb API
-        axios
-          .get(googleMapsURL)
-          .then((response) => {
-            console.log(response.data);
-            const tZone = response.data.timeZoneId;
+        const googleMapsRequest = await axios.get(googleMapsURL);
+        console.log(googleMapsRequest.data);
+        const tZone = googleMapsRequest.data.timeZoneId;
+        setTimeZone(tZone);
 
-
-            console.log(tZone);
-            let dateWithoutTZone = new Date(); // Get the current date and time
-            console.log(dateWithoutTZone);
-            let currentDate = new Date(dateWithoutTZone.toLocaleString("en-US", {
-              timeZone: tZone,
-            })); //date in the local timezone of the zipcode
-            console.log(currentDate);
-            let currentHour = currentDate.getHours(); // Get the current hour (0-23)
-            if (currentHour < 23 && currentHour > 5) {
-              // Set the time to 11:00pm (23:00) if it is between 5am and 11pm
-              console.log("between 5am and 11pm");
-              currentDate.setHours(23);
-            }
-
-            let isoDateTime = currentDate.toISOString(); // Get the ISO date-time string
-            console.log(isoDateTime);
-
-            // Get the visible objects at the zip code and at 9pm/current time using the visibleplanets API
-            const visible_planets_url = `https://api.visibleplanets.dev/v3?latitude=${lat}&longitude=${lng}&time=${isoDateTime}`;
-            axios
-              .get(visible_planets_url)
-              .then((response) => {
-                setVisibleObjects(response.data.data);
-                console.log(response.data.data);
-              })
-              .catch((error) => {
-                console.log(error);
-              });
+        console.log(tZone);
+        let dateWithoutTZone = new Date(); // Get the current date and time
+        console.log(dateWithoutTZone);
+        let currentDate = new Date(
+          dateWithoutTZone.toLocaleString("en-US", {
+            timeZone: tZone,
           })
-          .catch((error) => {
-            console.log(error);
-          });
-      })
-      .catch((error) => {
+        ); //date in the local timezone of the zipcode
+        console.log(currentDate);
+
+        
+        let currentHour = currentDate.getHours(); // Get the current hour (0-23)
+        if (currentHour < 23 && currentHour > 5) {
+          // Set the time to 11:00pm (23:00) if it is between 5am and 11pm
+          console.log("between 5am and 11pm");
+          currentDate.setHours(23);
+        }
+
+        let isoDateTime = currentDate.toISOString(); // Get the ISO date-time string
+        console.log(isoDateTime);
+
+        // Get the visible objects at the zip code and at 9pm/current time using the visibleplanets API
+        const visible_planets_url = `https://api.visibleplanets.dev/v3?latitude=${lat}&longitude=${lng}&time=${isoDateTime}`;
+        const response2 = await axios.get(visible_planets_url);
+        setVisibleObjects(response2.data.data);
+        console.log(response2.data.data);
+      } catch (error) {
         console.log(error);
-      });
+      }
+    };
+
+    fetchData();
   }, [zipCode]);
 
   return (
     <div>
       <h1>Tonight's Visibility</h1>
       {/* weather stuff*/}
+      <Weather latitude = {latitude} longitude = {longitude} timeZone = {timeZone} />
+
+
 
       <h2>Enter your zip code to see what you can see tonight!</h2>
       <input
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -2,39 +2,51 @@ import React from "react";
 import axios from "axios";
 import { useState, useEffect } from "react";
 
-function Weather({zipcode}) {
-    const [weather, setWeather] = useState([]);
-    const [zipCode, setZipCode] = useState(zipcode);
-    
-    const weather_url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${process.env.REACT_APP_OPEN_WEATHER_MAP_KEY}&units=imperial`;
-    
-    useEffect(() => {
-        if (zipCode.length !== 5) {
-        return
-        }
-        axios
-        .get(weather_url)
-        .then((response) => {
-            setWeather(response.data);
-            console.log(response.data);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-    }, [zipCode]);
-    
-    return (
-        <div className="weather">
-        <h2>Weather</h2>
-        <div className="weather-info">
-            <p>Temperature: {weather.main?.temp}°F</p>
-            <p>Feels Like: {weather.main?.feels_like}°F</p>
-            <p>Humidity: {weather.main?.humidity}%</p>
-            <p>Wind Speed: {weather.wind?.speed} mph</p>
-        </div>
-        </div>
-    );
+function Weather({ latitude, longitude, timeZone }) {
+  const [weather, setWeather] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+
+      const getGridURL = `https://api.weather.gov/points/${latitude},${longitude}`;
+      const gridRequest = await axios.get(getGridURL);
+      const hourlyForecastURL = gridRequest.data.properties.forecastHourly;
+      const hourlyForecastRequest = await axios.get(hourlyForecastURL);
+      const periods = hourlyForecastRequest.data.properties.periods;
+
+      let idealPeriod = periods[0];
+      if (idealPeriod.isDaytime && new Date(idealPeriod.startTime).getHours() < 23) {
+        for (let i = 0; i < periods.length; i++) {
+          if (new Date(periods[i].startTime).getHours() === 23) {
+            idealPeriod = periods[i];
+            break;
+          }
+        }
+      }
+
+      setWeather(idealPeriod);
+      console.log (weather);
+      
+    };
+    fetchData();
+  }, [latitude, longitude, timeZone]);
+
+  return (
+    <div className="weather">
+      <h2>Tonight's Weather</h2>
+      <div className="weather-info">
+        <h3>{weather.shortForecast}</h3>
+        <p>Temperature: {weather.temperature}°F</p>
+        {weather && weather.relativeHumidity && (
+          <p>Relative Humidity: {weather.relativeHumidity.value}%</p>
+        )}
+        {weather && weather.probabilityOfPrecipitation && (weather.probabilityOfPrecipitation.value > 10) && (
+          <p>Chance of Precipitation : {weather.probabilityOfPrecipitation.value}%</p>
+        )}
+        <p>Wind Speed: {weather.windSpeed}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Weather;
